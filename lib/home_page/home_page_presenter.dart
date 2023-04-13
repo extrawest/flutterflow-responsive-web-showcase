aef524f03501da6325f4a297a4e1e3d1acc19f74
Ruslan Volkhov <>
2023-04-13 17:08:07 +0300

    removed comment

--- lib/home_page/home_page_presenter.dart
+++ lib/home_page/home_page_presenter.dart
@@ -1,23 +1,3 @@
-// ResponsiveBuilder(
-//     builder: (context, sizingInformation) {
-//       // Check the sizing information here and return your UI
-//           if (sizingInformation.deviceScreenType == DeviceScreenType.desktop) {
-//           return Container(color:Colors.blue);
-//         }
-
-//         if (sizingInformation.deviceScreenType == DeviceScreenType.tablet) {
-//           return Container(color:Colors.red);
-//         }
-
-//         if (sizingInformation.deviceScreenType == DeviceScreenType.watch) {
-//           return Container(color:Colors.yellow);
-//         }
-
-//         return Container(color:Colors.purple);
-//       },
-//     },
-//   );
-
 import 'package:flutter/material.dart';
 import 'package:responsive_builder/responsive_builder.dart';
 import 'home_page_widget.dart';
